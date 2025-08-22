f839949f49d97c8b86e51c7441f160a2a5544f9e
Choong Jing Xiang <e1384379@u.nus.edu>
2025-08-22 17:38:28 +0800

    Added Increment Level-1

--- src/main/java/Eloise.java
+++ src/main/java/Eloise.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Eloise {
 
     private static final String line  = "_".repeat(50);
 
+    private static void msgBox(String msg) {
+        System.out.println(line);
+        System.out.println(" " + msg);
+        System.out.println(line);
+
+    }
     public static void main(String[] args) {
-        System.out.println(line);
-        System.out.println("Hello! I'm Eloise");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        msgBox("Hello, I'm Eloise!\n What can I do for you today?");
+
+        //checks for input
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String userInput = sc.nextLine(); //gives the actual input
+            if (userInput.equals("bye")) {
+                msgBox("Bye! Hope to see you again!");
+                break;
+            }
+
+            msgBox(userInput);
+        }
+
     }
 }
