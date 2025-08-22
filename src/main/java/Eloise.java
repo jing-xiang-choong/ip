d54eb3e0385da63fc454236c591318c2f815dd3c
Choong Jing Xiang <e1384379@u.nus.edu>
2025-08-22 16:40:18 +0800

    Added Increment Level-0

---
+++ src/main/java/Eloise.java
@@ -0,0 +1,13 @@
+public class Eloise {
+
+    private static final String line  = "_".repeat(50);
+
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println("Hello! I'm Eloise");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
